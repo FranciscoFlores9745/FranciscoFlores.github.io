692995fd4e607681c24d35eab6b018a83c457116
Francisco Flores <frfl9745@colorado.edu>
2025-09-20 00:05:04 -0600

    update wa5

--- WA5/wa5.js
+++ WA5/wa5.js
@@ -1,13 +1,25 @@
-const navToggle = document.querySelector('nav-toggle');
-const navMenu = document.querySelector('nav-menu');
-const navBurger = document.querySelector('.nav-burger-menu')
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+const navBurger = document.querySelector('.span');
 
 
 let toggle = false;
 
 
-navToggle.addEventListener("click",(e) => navBurger ){
+navToggle.addEventListener('click',showMenu );
 
+function showMenu(){
+    navMenu.classList.toggle("show");
+    navMenu.classList.toggle("hide");
+
+   /* if(toggle==false){
+    navMenu.classList.toggle("show");
+    toggle=true;
+    }
+    else{
+    navMenu.classList.toggle("hide");
+    toggle=false;
+    }*/
  
 
 }
