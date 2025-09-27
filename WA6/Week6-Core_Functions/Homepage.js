b51b0b08abd38a98c8f88b8ab8b175941250f24f
Francisco Flores <frfl9745@colorado.edu>
2025-09-26 22:38:32 -0600

    update core-functions

--- WA6/Week6-Core_Functions/Homepage.js
+++ WA6/Week6-Core_Functions/Homepage.js
@@ -4,43 +4,97 @@ const navBurger = document.querySelectorAll('span');
 
 
 let toggle = false;
+/* This is for the look of the hamburger menu and also its function so that you can access other pages on smaller screens, but to also show the user that it is
+working as intended*/
 
+navToggle.addEventListener('click', showMenu);
 
-navToggle.addEventListener('click',showMenu);
-
-function showMenu(){
+function showMenu() {
     navMenu.classList.toggle("show");
-    navBurger.forEach( span =>{
-    span.classList.toggle("open");
+    navBurger.forEach(span => {
+        span.classList.toggle("open");
     })
 
     navMenu.classList.toggle("hide");
-    navBurger.forEach( span =>{
-    span.classList.toggle("close");
+    navBurger.forEach(span => {
+        span.classList.toggle("close");
     })
 
 
-    if(toggle==false){
-    document.getElementById('aria-expanded').setAttribute('aria-label','Close navigation menu')
-    toggle=true;
+    if (toggle == false) {
+        document.getElementById('aria-expanded').setAttribute('aria-label', 'Close navigation menu')
+        toggle = true;
     }
-    else{
-    document.getElementById('aria-expanded').setAttribute('aria-label','Toggle navigation menu')
-    toggle=false;
+    else {
+        document.getElementById('aria-expanded').setAttribute('aria-label', 'Toggle navigation menu')
+        toggle = false;
     }
- 
+
 
 }
 
-document.addEventListener('click',(e)=>{
-    if((!navMenu.contains(e.target)&&!navToggle.contains(e.target))&&toggle==true){
+document.addEventListener('click', (e) => {
+    if ((!navMenu.contains(e.target) && !navToggle.contains(e.target)) && toggle == true) {
 
-        toggle=false;
-         document.getElementById('aria-expanded').setAttribute('aria-label','Toggle navigation menu')
+        toggle = false;
+        document.getElementById('aria-expanded').setAttribute('aria-label', 'Toggle navigation menu')
         navMenu.classList.toggle("hide");
-        navBurger.forEach( span =>{
-        span.classList.toggle("close");
+        navBurger.forEach(span => {
+            span.classList.toggle("close");
         })
 
     }
 })
+/* This is for the filters, this makes sure that when certain things are checked off, that it will hide or show the information that the user chose*/
+const filterButtons = document.querySelectorAll('.info-nav button');
+const photoCards = document.querySelectorAll('.content');
+
+
+// Button filters
+filterButtons.forEach(button => {
+
+    button.addEventListener('click', (event) => {
+        const filterValue = event.target.textContent.toLowerCase();
+        filterPhotos(filterValue);
+
+        /*alert(filterValue);*/
+
+
+        function filterPhotos(category) {
+            number = 0;
+            photoCards.forEach(card => {
+
+
+                if (category === 'all' || card.dataset.catagory === category) {
+                    number++;
+                    console.log(card.dataset.catagory)
+                    card.classList.toggle('block');
+                    card.style.opacity = 100;
+                    setTimeout(() => card.style.display = 'block', 500);
+                    if (card.style.color == 'red') {
+                        card.style.color = 'black'
+                    } else {
+                        card.style.color = 'red'
+                    }
+                    document.getElementById("Photos").innerText = "#Photos: " + number;
+
+                } else {
+
+                    console.log(card.dataset.catagory)
+                    card.classList.toggle('none');
+                    card.style.opacity = 0;
+                    setTimeout(() => card.style.display = 'none', 500);
+                    if (card.style.color == 'black') {
+                        card.style.color = 'red'
+                    } else {
+                        card.style.color = 'black'
+                    }
+
+                }
+            });
+        }
+
+    });
+
+
+});
--- WA6/Week6-Core_Functions/Resources.js
+++ WA6/Week6-Core_Functions/Resources.js
@@ -4,42 +4,43 @@ const navBurger = document.querySelectorAll('span');
 
 
 let toggle = false;
+/* This is for the look of the hamburger menu and also its function so that you can access other pages on smaller screens, but to also show the user that it is
+working as intended*/
 
+navToggle.addEventListener('click', showMenu);
 
-navToggle.addEventListener('click',showMenu);
-
-function showMenu(){
+function showMenu() {
     navMenu.classList.toggle("show");
-    navBurger.forEach( span =>{
-    span.classList.toggle("open");
+    navBurger.forEach(span => {
+        span.classList.toggle("open");
     })
 
     navMenu.classList.toggle("hide");
-    navBurger.forEach( span =>{
-    span.classList.toggle("close");
+    navBurger.forEach(span => {
+        span.classList.toggle("close");
     })
 
 
-    if(toggle==false){
-    document.getElementById('aria-expanded').setAttribute('aria-label','Close navigation menu')
-    toggle=true;
+    if (toggle == false) {
+        document.getElementById('aria-expanded').setAttribute('aria-label', 'Close navigation menu')
+        toggle = true;
     }
-    else{
-    document.getElementById('aria-expanded').setAttribute('aria-label','Toggle navigation menu')
-    toggle=false;
+    else {
+        document.getElementById('aria-expanded').setAttribute('aria-label', 'Toggle navigation menu')
+        toggle = false;
     }
- 
+
 
 }
 
-document.addEventListener('click',(e)=>{
-    if((!navMenu.contains(e.target)&&!navToggle.contains(e.target))&&toggle==true){
+document.addEventListener('click', (e) => {
+    if ((!navMenu.contains(e.target) && !navToggle.contains(e.target)) && toggle == true) {
 
-        toggle=false;
-         document.getElementById('aria-expanded').setAttribute('aria-label','Toggle navigation menu')
+        toggle = false;
+        document.getElementById('aria-expanded').setAttribute('aria-label', 'Toggle navigation menu')
         navMenu.classList.toggle("hide");
-        navBurger.forEach( span =>{
-        span.classList.toggle("close");
+        navBurger.forEach(span => {
+            span.classList.toggle("close");
         })
 
     }
