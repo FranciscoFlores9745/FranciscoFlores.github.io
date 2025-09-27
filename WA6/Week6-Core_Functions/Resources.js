1fb38eea72f5bd86468fe429f512042240781286
Francisco Flores <frfl9745@colorado.edu>
2025-09-26 20:28:57 -0600

    update Core-Functions

---
+++ WA6/Week6-Core_Functions/Resources.js
@@ -0,0 +1,46 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+const navBurger = document.querySelectorAll('span');
+
+
+let toggle = false;
+
+
+navToggle.addEventListener('click',showMenu);
+
+function showMenu(){
+    navMenu.classList.toggle("show");
+    navBurger.forEach( span =>{
+    span.classList.toggle("open");
+    })
+
+    navMenu.classList.toggle("hide");
+    navBurger.forEach( span =>{
+    span.classList.toggle("close");
+    })
+
+
+    if(toggle==false){
+    document.getElementById('aria-expanded').setAttribute('aria-label','Close navigation menu')
+    toggle=true;
+    }
+    else{
+    document.getElementById('aria-expanded').setAttribute('aria-label','Toggle navigation menu')
+    toggle=false;
+    }
+ 
+
+}
+
+document.addEventListener('click',(e)=>{
+    if((!navMenu.contains(e.target)&&!navToggle.contains(e.target))&&toggle==true){
+
+        toggle=false;
+         document.getElementById('aria-expanded').setAttribute('aria-label','Toggle navigation menu')
+        navMenu.classList.toggle("hide");
+        navBurger.forEach( span =>{
+        span.classList.toggle("close");
+        })
+
+    }
+})
